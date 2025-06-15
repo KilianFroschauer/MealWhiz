c0cec43db909de601d208d78ceb13958bcb9699a
Adrian Pichler <pichleradrian292@gmail.com>
2025-06-15 16:59:40 +0200

    remove config files

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var MealWhizConfig;
-(function (MealWhizConfig) {
-    MealWhizConfig.apiBaseURL = "https://mealwhiz.at:3000";
-    MealWhizConfig.jitsiServerURL = "https://meet.mealwhiz.at";
-})(MealWhizConfig || (MealWhizConfig = {}));
--- assets/ts/config.ts
+++
@@ -1,4 +0,0 @@
-namespace MealWhizConfig {
-    export const apiBaseURL = "https://mealwhiz.at:3000";
-    export const jitsiServerURL = "https://meet.mealwhiz.at";
-}
